4befbfa624218311550d6889e666e06778fe3e6b
waceho <amancio.perros@gmail.com>
2018-06-08 17:37:42 +0200

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,40 +1,15 @@
-import BaseJobBuilder
-
-String directory = 'Job-eval'
-folder(directory) {
-    description 'contient les jobs à exécuter'
-}
-
-[
-    [
-        name: 'job_version_1',
-        repository: 'https://github.com/waceho/eval_devops2.git',
-        branch: 'V1',
-        gitTag:'GIT_TAG_1',
-        script:'start.sh'
-    ],
-    [
-        name: 'job_version_2',
-        repository: 'https://github.com/waceho/eval_devops2.git',
-        branch: 'V2',
-        gitTag:'GIT_TAG_2',
-        script:'start.sh'
-    ],
-    [
-        name: 'job_version_3',
-        repository: 'https://github.com/waceho/eval_devops2.git',
-        branch: 'V3',
-        gitTag:'GIT_TAG_3',
-        script:'start.sh'
-    ]
-].each { Map config ->
-    new BaseJobBuilder(
-        directory: directory,
-        name: config.name,
-        repository: config.repository,
-        branch: config.branch,
-        gitTag: config.gitTag,
-        script: config.script,
-        gradleTasks: 'version builded'
-    ).build(this)
+def project = 'waceho/eval_devops2'
+def branchApi = new URL("https://api.github.com/repos/${project}/branches")
+def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
+branches.each {
+    def branchName = it.name
+    def jobName = "${project}-${branchName}".replaceAll('/','-')
+    job(jobName) {
+        scm {
+            git("git://github.com/${project}.git", branchName)
+        }
+        steps {
+            maven("test -Dproject.name=${project}/${branchName}")
+        }
+    }
 }
